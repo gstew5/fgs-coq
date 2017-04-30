d888194f547b8dce8483418a6e8847796592c254
Gordon Stewart <gstew5@gmail.com>
2017-04-30 17:21:25 -0400

    Coordination commit

--- components.v
+++ components.v
@@ -1,5 +1,7 @@
 Require Import Arith Omega List.
 
+(** The *)
+
 Definition is_true : bool -> Prop := fun b => b = true.
 Coercion is_true : bool >-> Sortclass.
 
@@ -46,6 +48,10 @@ Module Type DELAY_N_BUS (V : VAL) (D : DELAY).
   Axiom init_state : state.
 
   (* R2: *)
+  Axiom value_init_state :
+    forall i (pf : i < delay), value i pf init_state = V.init.
+
+  (* R3: *)
   Axiom step_values :
     forall (v_in : val)
            (s_in : state),
@@ -56,7 +62,7 @@ Module Type DELAY_N_BUS (V : VAL) (D : DELAY).
           value (S i) pf s_out = v_i.
 End DELAY_N_BUS.
 
-Module DelayNBus (V : VAL) (D : DELAY).
+Module DelayNBus (V : VAL) (D : DELAY) : DELAY_N_BUS V D.
   Notation val := V.t.
   Notation delay := D.n.
 
@@ -65,6 +71,10 @@ Module DelayNBus (V : VAL) (D : DELAY).
     match s with
     | exist _ l _ => l
     end.
+
+  Program Definition init_state : state :=
+    exist _ (List.repeat V.init delay) _.
+  Next Obligation. apply repeat_length. Qed.
   
   Lemma cons_length {A} x (l : list A) : length (x :: l) = S (length l).
   Proof. reflexivity. Qed.
@@ -100,7 +110,14 @@ Module DelayNBus (V : VAL) (D : DELAY).
 
   Definition value i (pf : i < delay) (s : state) : val :=
     List.nth i (list_of_state s) V.init.
-  
+
+  Lemma value_init_state :
+    forall i (pf : i < delay), value i pf init_state = V.init.
+  Proof.
+    intros i pf; unfold value, init_state, list_of_state.
+    clear pf; revert i; induction delay; simpl; destruct i; auto.
+  Qed.
+    
   Lemma step_values : 
     forall (v_in : val)
            (s_in : state),
@@ -177,6 +194,8 @@ Module Delay1Bus_of_DelayNBus (V : VAL) : DELAY_1_BUS V.
   Lemma init_state_output : output init_state = V.init.
   Proof.
     unfold init_state, output; simpl.
+    rewrite Delay1Bus.value_init_state; auto.
+  Qed.    
 
   Lemma step_output :
     forall (v_in : val) (s_in : state),
@@ -414,9 +433,9 @@ Module PilotFlyingSystem_Side (V : VAL) (P : PRIMARY_SIDE)
   Proof. auto. Qed.
 End PilotFlyingSystem_Side.
 
-Module BoolInitHigh : VAL.
+Module BoolInitHigh <: VAL.
   Definition t := bool.                          
-  Definition init_t := true.
+  Definition init := true.
   Definition is_true (b : t) : bool := b.
   Definition high := true.
   Definition low := false.
@@ -426,9 +445,9 @@ Module BoolInitHigh : VAL.
   Proof. unfold is_true, low; reflexivity. Qed.
 End BoolInitHigh.    
 
-Module BoolInitLow : VAL.
+Module BoolInitLow <: VAL.
   Definition t := bool.                          
-  Definition init_t := false.
+  Definition init := false.
   Definition is_true (b : t) : bool := b.
   Definition high := true.
   Definition low := false.
@@ -439,10 +458,10 @@ Module BoolInitLow : VAL.
 End BoolInitLow.    
 
 Module PilotFlyingSystem.
-  Module LeftSidePrimary : PRIMARY_SIDE. Definition is_primary := true.
+  Module LeftSidePrimary <: PRIMARY_SIDE. Definition is_primary := true.
   End LeftSidePrimary.
 
-  Module RightSideNotPrimary : PRIMARY_SIDE. Definition is_primary := false.
+  Module RightSideNotPrimary <: PRIMARY_SIDE. Definition is_primary := false.
   End RightSideNotPrimary.
     
   Module Left_Side := PilotFlyingSystem_Side BoolInitHigh LeftSidePrimary.
@@ -457,7 +476,9 @@ Module PilotFlyingSystem.
         right_side : Right_Side.state;
 
         lr_bus : LR_Bus.state;
-        rl_bus : RL_Bus.state
+        rl_bus : RL_Bus.state;
+
+        pre_TS : bool                   
       }.
 
   Definition init_state : state :=
@@ -465,18 +486,285 @@ Module PilotFlyingSystem.
       Left_Side.init_state
       Right_Side.init_state
       LR_Bus.init_state
-      RL_Bus.init_state.
+      RL_Bus.init_state
+      true.
 
   (* State transition relation *)
-  Definition step (s : state) : state :=
-    let 
+  Section step.
+    Variables (s : state) (ts : bool).
+
+    (* Update LR_Bus and Right_Side *)
+    Definition c1 := Left_Side.pilot_flying (left_side s).
+    Definition next_LR := LR_Bus.step c1 (lr_bus s).
+    Definition c2 := LR_Bus.output next_LR.
+    Definition next_RS := Right_Side.step ts c2 (right_side s).
+    
+    (* Update RL_Bus and Left_Side *)    
+    Definition c3 := Right_Side.pilot_flying (right_side s).
+    Definition next_RL := RL_Bus.step c3 (rl_bus s).
+    Definition c4 := RL_Bus.output next_RL.
+    Definition next_LS := Left_Side.step ts c4 (left_side s).
+    
+    Definition step : state := 
+      (* Return the resulting state *)
+      mkState
+        next_LS
+        next_RS
+        next_LR
+        next_RL
+        ts.
+
+    Lemma c1_c2 : c1 = c2.
+    Proof. unfold c2, next_LR; rewrite LR_Bus.step_output; auto. Qed.
+
+    Lemma c3_c4 : c3 = c4.
+    Proof. unfold c4, next_RL; rewrite RL_Bus.step_output; auto. Qed.
+  End step.    
   
   (* Observable behaviors: *)
-  Definition left_pilot_flying_side (s : state) : bool :=
+  Definition left_pilot_flying (s : state) : bool :=
     Left_Side.pilot_flying (left_side s).
-  Definition right_pilot_flying_side (s : state) : bool :=
+  Definition right_pilot_flying (s : state) : bool :=
     Right_Side.pilot_flying (right_side s).
 End PilotFlyingSystem.
+
+Module SystemRequirements.
+  Import PilotFlyingSystem.
+
+  Inductive step_star : state -> state -> Prop := 
+  | step_refl : forall s, step_star s s
+  | step_trans :
+      forall s s' ts,
+        let s'' := step s ts in 
+        step_star s'' s' ->
+        step_star s s'.
+
+  Inductive reachable : state -> Prop :=
+  | reachable_init : reachable init_state
+  | reachable_step_star :
+      forall s s',
+        reachable s ->
+        step_star s s' ->
+        reachable s'.
+
+  Inductive valid : state -> Prop :=
+  | mkValid :
+      forall s,
+        (* pre_TS consistency *)
+        Left_Side.pre_TS (left_side s) = pre_TS s ->
+        Right_Side.pre_TS (right_side s) = pre_TS s ->        
+
+        (* pre_OSPF consistency *)
+        Left_Side.pre_OSPF (left_side s) = RL_Bus.output (rl_bus s) ->
+        Right_Side.pre_OSPF (right_side s) = LR_Bus.output (lr_bus s) ->        
+
+        (* at least one side is flying *)
+        left_pilot_flying s \/ right_pilot_flying s ->
+
+        (* buses differ when sides same *)
+        (left_pilot_flying s = right_pilot_flying s ->
+         LR_Bus.output (lr_bus s) = negb (RL_Bus.output (rl_bus s))) -> 
+        
+        valid s.
+
+  Lemma rise_right_ospf_left_pilot_flying :
+    forall s ts,
+      Left_Side.pre_OSPF (left_side s) = RL_Bus.output (rl_bus s) ->
+      Right_Side.pre_OSPF (right_side s) = LR_Bus.output (lr_bus s) ->
+      right_pilot_flying s ->            
+      (left_pilot_flying s = right_pilot_flying s ->
+       LR_Bus.output (lr_bus s) = negb (RL_Bus.output (rl_bus s))) ->
+      Right_Side.rise_ospf (right_side s) (c2 s) = true ->
+      Left_Side.pilot_flying (Left_Side.step ts (c4 s) (left_side s)).
+  Proof.
+    intros s ts H2 H3 H4 H5 H6.
+    unfold Right_Side.rise_ospf, Right_Side.rise in H6.
+    rewrite Bool.andb_true_iff in H6; destruct H6 as [H6 H7].
+    rewrite <-c1_c2 in H7; unfold c1 in H7.
+    rewrite Left_Side.hlr5; try reflexivity; auto.
+    unfold Left_Side.rise_ospf, Left_Side.rise.
+    apply Bool.andb_false_intro1.
+    unfold Left_Side.is_true.
+    rewrite H2; rewrite H3 in H6; rewrite <-H5.
+    { unfold Right_Side.is_true in H6; generalize H6.
+      destruct (LR_Bus.output _); auto. }
+    rewrite H4; unfold left_pilot_flying; rewrite H7; auto. 
+  Qed.
+
+  Lemma rise_left_ospf_right_pilot_flying :
+    forall s ts,
+      Left_Side.pre_OSPF (left_side s) = RL_Bus.output (rl_bus s) ->
+      Right_Side.pre_OSPF (right_side s) = LR_Bus.output (lr_bus s) ->
+      left_pilot_flying s ->            
+      (left_pilot_flying s = right_pilot_flying s ->
+       LR_Bus.output (lr_bus s) = negb (RL_Bus.output (rl_bus s))) ->
+      Left_Side.rise_ospf (left_side s) (c4 s) = true ->
+      Right_Side.pilot_flying (Right_Side.step ts (c2 s) (right_side s)).
+  Proof.
+    intros s ts H2 H3 H4 H5 H6.
+    unfold Left_Side.rise_ospf, Left_Side.rise in H6.
+    rewrite Bool.andb_true_iff in H6; destruct H6 as [H6 H7].
+    rewrite <-c3_c4 in H7; unfold c3 in H7.
+    rewrite Right_Side.hlr5; try reflexivity; auto.
+    unfold Right_Side.rise_ospf, Right_Side.rise.
+    apply Bool.andb_false_intro1.
+    rewrite H3; rewrite H2 in H6; rewrite H5.
+    { unfold Left_Side.is_true in H6; rewrite H6; auto. }
+    rewrite H4; unfold right_pilot_flying; rewrite H7; auto. 
+  Qed.
+  
+  Lemma valid_step_valid :
+    forall s ts,
+      valid s ->
+      valid (step s ts).
+  Proof.
+    (* surely automatable with some effort *)
+    intros s ts; inversion 1; subst; constructor.
+    { unfold step, next_LS; simpl; rewrite Left_Side.hlr8; auto. }
+    { unfold step, next_RS; simpl; rewrite Right_Side.hlr8; auto. }
+    { unfold step, next_LS; simpl. rewrite Left_Side.hlr9; auto. }
+    { unfold step, next_RS; simpl. rewrite Right_Side.hlr9; auto. }    
+    { destruct H4 as [H4|H4]; unfold step, left_pilot_flying, right_pilot_flying; simpl.
+      { unfold next_LS, next_RS.
+        destruct (Left_Side.rise_ospf (left_side s) (c4 s)) eqn:H6.
+        { right; apply rise_left_ospf_right_pilot_flying; auto. }
+        left; rewrite Left_Side.hlr5; try reflexivity; auto. }
+      unfold next_LS, next_RS.
+      destruct (Right_Side.rise_ospf (right_side s) (c2 s)) eqn:H6.
+      { left; apply rise_right_ospf_left_pilot_flying; auto. }
+      right; rewrite Right_Side.hlr5; try reflexivity; auto. }
+    unfold left_pilot_flying, right_pilot_flying, step; simpl.
+    unfold next_LS, next_RS, next_RL, next_LR.
+    rewrite RL_Bus.step_output, LR_Bus.step_output; intros H6.
+    unfold c1, c3; destruct H4 as [H4|H4].
+    { destruct (Left_Side.rise_ospf (left_side s) (c4 s)) eqn:H7.
+      { assert (H8: Right_Side.pilot_flying (Right_Side.step ts (c2 s) (right_side s))).
+        { apply rise_left_ospf_right_pilot_flying; auto. }
+        rewrite <-H6 in H8; rewrite Left_Side.hlr4 in H8; auto; inversion H8. }
+      rewrite Left_Side.hlr5 in H6; auto.
+      unfold left_pilot_flying in H4; rewrite H4.
+      destruct (Right_Side.pilot_flying (right_side s)) eqn:H8; auto.
+      assert (H9: Right_Side.rise_ospf (right_side s) (c2 s) = false).
+      { destruct (Right_Side.rise_ospf _ _) eqn:H9; auto.
+        rewrite Right_Side.hlr4 in H6; auto. }
+      unfold Left_Side.rise_ospf, Left_Side.rise, Left_Side.is_true in H7.
+      unfold Right_Side.rise_ospf, Right_Side.rise, Right_Side.is_true in H9.
+      rewrite H2 in H7; rewrite H3 in H9; rewrite H5 in H9.
+      { unfold c4, next_RL in H7; rewrite RL_Bus.step_output in H7.
+        unfold c2, next_LR in H9; rewrite LR_Bus.step_output in H9.
+        unfold c3 in H7; unfold c1 in H9; rewrite H8 in H7; rewrite H4 in H9.
+        rewrite Bool.negb_involutive, Bool.andb_comm in H9; simpl in H9.
+        rewrite H9 in H7; auto. }
+      unfold left_pilot_flying, right_pilot_flying; rewrite H4, H8; auto. }
+    destruct (Right_Side.rise_ospf (right_side s) (c2 s)) eqn:H7.
+    { assert (H8: Left_Side.pilot_flying (Left_Side.step ts (c4 s) (left_side s))).
+      { apply rise_right_ospf_left_pilot_flying; auto. }
+      rewrite H6 in H8; rewrite Right_Side.hlr4 in H8; auto; inversion H8. }
+    rewrite Right_Side.hlr5 in H6; auto.
+    unfold right_pilot_flying in H4; rewrite H4.
+    destruct (Left_Side.pilot_flying (left_side s)) eqn:H8; auto.
+    assert (H9: Left_Side.rise_ospf (left_side s) (c4 s) = false).
+    { destruct (Left_Side.rise_ospf _ _) eqn:H9; auto.
+      rewrite Left_Side.hlr4 in H6; auto. }
+    unfold Right_Side.rise_ospf, Right_Side.rise, Right_Side.is_true in H7.
+    unfold Left_Side.rise_ospf, Left_Side.rise, Left_Side.is_true in H9.    
+    rewrite H2 in H9; rewrite H3 in H7; rewrite <-H5 in H9.
+    { unfold c4, next_RL in H9; rewrite RL_Bus.step_output in H9.
+      unfold c2, next_LR in H7; rewrite LR_Bus.step_output in H7.
+      unfold c3 in H9; unfold c1 in H7; rewrite H8 in H7; rewrite H4 in H9.
+      rewrite Bool.andb_comm in H9; simpl in H9; rewrite H9 in H7; auto. }
+    unfold left_pilot_flying, right_pilot_flying; rewrite H4, H8; auto. 
+  Qed.      
+
+  Lemma valid_step_star_valid s s' :
+    step_star s s' ->
+    valid s ->
+    valid s'.
+  Proof.
+    induction 1; auto; intros Hval.
+    apply IHstep_star.
+    apply valid_step_valid; auto.
+  Qed.
+
+  Lemma init_valid : valid init_state.
+  Proof.
+    unfold init_state; constructor; simpl; auto.
+    { generalize Left_Side.hlr2; auto. }
+    { generalize Right_Side.hlr2; auto. }
+    { generalize Left_Side.hlr3; unfold Left_Side.is_true; intros ->.
+      rewrite RL_Bus.init_state_output; auto. }
+    { generalize Right_Side.hlr3; unfold Right_Side.is_true; intros ->.
+      rewrite LR_Bus.init_state_output; auto. }
+    { left; unfold left_pilot_flying; simpl; rewrite Left_Side.hlr1; reflexivity. }
+    unfold left_pilot_flying; simpl; rewrite Left_Side.hlr1.
+    unfold right_pilot_flying; simpl; rewrite Right_Side.hlr1; inversion 1.
+  Qed.    
+                       
+  Lemma reachable_valid s :
+    reachable s ->
+    valid s.
+  Proof.
+    induction 1; subst.
+    { apply init_valid. }
+    eapply valid_step_star_valid; eauto.
+  Qed.    
+
+  (** High-level System Requirements *)
+  
+  Definition pressed (s : state) (ts : bool) := 
+    pre_TS s = false /\ ts = true.
+
+  Definition switching_sides (s : state) :=
+    (left_pilot_flying s /\ LR_Bus.output (lr_bus s) = false) \/
+    (right_pilot_flying s /\ RL_Bus.output (rl_bus s) = false).
+
+  Theorem at_least_one_pilot_flying :
+    forall s,
+      reachable s ->
+      left_pilot_flying s \/ right_pilot_flying s.
+  Proof.
+    intros s H; generalize (reachable_valid _ H).
+    inversion 1; subst; auto.
+  Qed.    
+
+  Theorem at_most_one_pilot_flying_except_when_switching_sides :
+    forall s,
+      reachable s -> 
+      ~switching_sides s ->
+      left_pilot_flying s = negb (right_pilot_flying s).
+  Proof.
+    intros s H H0; generalize (reachable_valid _ H); inversion 1; subst.
+    destruct H6.
+    { destruct (right_pilot_flying s) eqn:H8; auto.
+      elimtype False; apply H0; unfold switching_sides.
+      rewrite H7, H6, H8; auto.
+      destruct (RL_Bus.output (rl_bus s)).
+      { left; split; reflexivity. }
+      right; split; reflexivity. }
+    rewrite H6; destruct (left_pilot_flying s) eqn:H8; auto.
+    elimtype False; apply H0; unfold switching_sides.
+    rewrite H6, H8, H7; auto.
+    destruct (RL_Bus.output (rl_bus s)).
+    { left; split; reflexivity. }
+    right; split; reflexivity. 
+  Qed.
+
+  Theorem pressing_transfer_changes_side_l2r : 
+    forall s ts,
+      reachable s ->
+      left_pilot_flying s ->
+      pressed s ts ->
+      right_pilot_flying (step s ts).
+  Proof.
+    intros s ts H H1 [H2 H3].
+    generalize (reachable_valid _ H); inversion 1; subst.
+    unfold right_pilot_flying, step; simpl; unfold next_RS.
+    
+    rewrite Right_Side.hlr6; try reflexivity.
+    
+    
+End SystemRequirements.    
+    
   
   
   
